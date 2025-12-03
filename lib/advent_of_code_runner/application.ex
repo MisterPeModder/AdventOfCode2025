e21548da06f270ff7dbe05aaf47045c8716e07ad
Yanis Guaye <yguaye44@gmail.com>
2025-12-04 00:27:14 +0100

    setup: automatically download puzzle input

--- lib/advent_of_code_runner/application.ex
+++ lib/advent_of_code_runner/application.ex
@@ -2,6 +2,9 @@ defmodule AdventOfCodeRunner.Application do
   alias AdventOfCodeRunner.{Solution, SolutionInfo}
   use Application
 
+  @cache_dir ".aoc"
+  @config_path "#{@cache_dir}/config.json"
+
   @impl true
   def start(_start_type, _start_args) do
     solution = AdventOfCode2025.Day01
@@ -10,19 +13,86 @@ defmodule AdventOfCodeRunner.Application do
 
     IO.puts("== Running Advent of Code #{year} - Day #{day} ==")
 
-    with {:ok, transformed} <- run_setup(solution, read_input()) do
-      run_part(1, solution, transformed, &Solution.part1/2)
-      run_part(2, solution, transformed, &Solution.part2/2)
-    end
+    File.mkdir_p!(@cache_dir)
+
+    token =
+      case read_token() do
+        {:ok, t} -> t
+        {:error, error} -> raise RuntimeError, "Failure to read token: #{error}"
+      end
+
+    input = fetch_input!(year, day, token)
+    run_aoc(solution, input)
 
     {:ok, self()}
   end
 
-  defp read_input() do
-    IO.puts("Reading input from cache: ./input.txt")
-    input = File.read!("input.txt")
-    IO.puts("Read input: #{byte_size(input)} bytes")
+  defp run_aoc(solution, input) do
+    with {:ok, transformed} <- run_setup(solution, input) do
+      run_part(1, solution, transformed, &Solution.part1/2)
+      run_part(2, solution, transformed, &Solution.part2/2)
+    end
+  end
+
+  defp read_token() do
+    case File.read(@config_path) do
+      {:ok, data} ->
+        case JSON.decode(data) do
+          {:ok, %{"token" => token}} when is_binary(token) -> {:ok, token}
+          {:ok, _} -> {:error, "Missing \"token\" string in configuration at #{@config_path}"}
+          error -> error
+        end
+
+      {:error, :enoent} ->
+        empty_config = JSON.encode_to_iodata!(%{token: ""})
+
+        if File.write(@config_path, empty_config) === :ok do
+          IO.puts("Created empty configuration at #{@config_path}")
+        end
+
+        {:error, :file.format_error(:enoent)}
+
+      {:error, error} ->
+        {:error, :file.format_error(error)}
+    end
+  end
+
+  defp fetch_input!(year, day, token) do
+    path = "#{@cache_dir}/input_#{year}_#{day}.txt"
+
+    IO.puts("Reading input from cache: #{path}")
+
+    input =
+      case File.read(path) do
+        {:ok, input} ->
+          IO.puts("Read input from cache: #{byte_size(input)} bytes")
+          input
+
+        {:error, :enoent} ->
+          download_input!(year, day, token)
+      end
+
+    File.write(path, input)
     input
+  end
+
+  defp download_input!(year, day, token) do
+    url = "https://adventofcode.com/#{year}/day/#{day}/input"
+    IO.puts("Downloading input from #{url}")
+
+    res =
+      Req.new(url: url)
+      |> Req.Request.put_header("cookie", "session=#{token}")
+      |> Req.request!()
+
+    case res do
+      %Req.Response{status: 200, body: input} ->
+        IO.puts("Downloaded input from #{url}: #{byte_size(input)} bytes")
+        input
+
+      %Req.Response{status: status} = res ->
+        raise "Received unexpected status #{status}: #{Map.get(res, :body, "(no body)")}"
+    end
   end
 
   defp run_setup(solution, input) do
